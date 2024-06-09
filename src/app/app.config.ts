d915f9030c617f31aa3966d770502da540f1dedf
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-06-09 15:07:58 +0900

    :art:

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()],
 };
