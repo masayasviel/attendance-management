a672cd65f1187e1301723a058dc3324fcb22b913
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-06-18 23:10:27 +0900

    📦 ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideAnimations(),
-  ],
+    provideStore()
+],
 };
