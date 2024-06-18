0af837c322ca45596c05111c619949bfe5a259d8
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-06-19 01:15:20 +0900

    :sparkles: button

--- src/app/internal/template/header/header.component.ts
+++ src/app/internal/template/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { InternalFacade } from '../../store/internal.facade';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private internalFacade = inject(InternalFacade);
+
+  /** 出勤 */
+  onClickAttendanceAtWorkButton(): void {
+    this.internalFacade.setAttendanceAtWork();
+  }
+
+  /** リセット */
+  onClickResetButton(): void {
+    this.internalFacade.reset();
+  }
+}
