e6e8eddf8174faba93029038a1e02815f03b8452
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-06-22 02:44:30 +0900

    :sparkles: 細かいところ詰めた

--- src/app/internal/template/template.component.ts
+++ src/app/internal/template/template.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterOutlet } from '@angular/router';
+import dayjs from 'dayjs';
 import { InternalFacade } from '../store/internal.facade';
 import { HeaderComponent } from './header/header.component';
 
@@ -17,11 +18,9 @@ export class TemplateComponent {
 
   /** 出勤 */
   attendanceAtWork(): void {
-    const signalRecord = this.internalFacade.signalRecords();
-    if (signalRecord.length > 0 && !signalRecord.at(-1)?.finish) {
-      this.snackbar.open('未退勤のレコードがあります。', 'Done', {
-        duration: 3000,
-      });
+    const message = this.isEnableAttendanceAtWorkGetAlertMessage();
+    if (message) {
+      this.snackbar.open(message, 'Done', { duration: 3000 });
       return;
     }
     this.internalFacade.setAttendanceAtWork();
@@ -30,5 +29,17 @@ export class TemplateComponent {
   /** リセット */
   reset(): void {
     this.internalFacade.reset();
+  }
+
+  /** 出勤できるか確認とできない場合はその理由 */
+  private isEnableAttendanceAtWorkGetAlertMessage(): string | null {
+    const signalRecord = this.internalFacade.signalRecords();
+    if (signalRecord.length > 0 && !signalRecord.at(-1)?.finish) {
+      return '未退勤のレコードがあります。';
+    }
+    if (signalRecord.length > 0 && signalRecord.at(-1)?.date === dayjs().format('YYYY/MM/DD')) {
+      return '本日はすでに出勤記録があります。';
+    }
+    return null;
   }
 }
